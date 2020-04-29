b23f7617493a31c66b603928260a612227ce2fe0
Kamil Szela <kamil.szela.programista@gmail.com>
2020-04-29 21:18:07 +0200

    register a user - validate inputs

--- register.php
+++ register.php
@@ -1,3 +1,61 @@
+<?php
+	session_start();
+	
+	if(isset($_POST['email'])){
+		$successful_validation = true;
+		
+		$login = $_POST['login'];
+		
+		if(strlen($login)<3 || strlen($login)>20){
+			$successful_validation = false;
+			$_SESSION['login_error']="Login musi posiadać od 3 do 20 znaków";
+		}
+		if(ctype_alnum($login) == false){
+			$successful_validation = false;
+			$_SESSION['login_error'] = "Login może składać się tylko z liter i cyfr (bez polskich znaków)";
+		}
+		
+		$email = $_POST['email'];
+		$email_After_Sanitization = filter_var($email, FILTER_SANITIZE_EMAIL);
+		$email_After_Validation = filter_var($email_After_Sanitization, FILTER_VALIDATE_EMAIL);
+		
+		if(($email_After_Validation == false) || ($email_After_Sanitization != $email)){
+			$wszystko_ok = false;
+			$_SESSION['email_error'] = "Podaj poprawny adres email";
+		}
+		
+		$password = $_POST['password'];
+		$password_Repeat = $_POST['password2'];
+		
+		if(strlen($password)<8 || strlen($password)>20){
+			$wszystko_ok = false;
+			$_SESSION['password_error'] = "Hasło musi posiadać od 8 do 20 znaków";
+		}
+		
+		if($password != $password_Repeat){
+			$wszystko_ok = false;
+			$_SESSION['password_error'] = "Podane hasła nie są identyczne";
+		}
+		
+		if($wszystko_ok == true){
+		
+			$haslo_hash = password_hash($password, PASSWORD_DEFAULT);
+			
+			require_once 'database.php';
+			
+			try{
+				$userQuery = $db->prepare('SELECT id FROM users WHERE email = :email');
+				$userQuery->bindValue(':email', $login, PDO::PARAM_STR);
+				$userQuery->execute();
+				
+			}
+			catch(PDOException $e){
+				echo "<span style=\"color:red;\">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestracje w innym terminie!</span>";
+			}
+		}
+	}
+	
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -24,40 +82,55 @@
 			<h3><b>Bezpożyczek.pl - aplikacja internetowa wspomagająca zażądzanie funduszami</b></h3>
 		</header>
 		<div class="row custom-bg py-2 align-items-center justify-content-center">
-			
-			<div class="col-sm-10 align-self-center h-500" id="userDataGroup">
+			<p class="font-2rem mt-3 mr-3 ml-3 text-center">Aby zarejestrowac się jako użytkownik wypełnij poniższy formularz</p>
+			<div class="col-sm-10 align-self-center h-register" id="userDataGroup">
 			<form method="post">
-				<div class="input-group mb-2">
+				<div class="input-group mb-4">
 					<div class="input-group-prepend">
-						<span class="fa fa-user input-group-text fa-22-font"></span>
+						<span class="fa fa-user input-group-text fa-22-font pt-11"></span>
 					</div>
-					<input class="form-control" type="text" name="login" placeholder="Login"></input>
+					<input class="form-control form-control-lg" type="text" name="login" placeholder="Login"></input>
+				</div>
+				<?php
+					if(isset($_SESSION['login_error'])){
+						echo '<p class="text-danger">'.$_SESSION['login_error'].'</p>';
+						unset($_SESSION['login_error']);
+					}
+				?>
+				<div class="input-group mb-4">
+					<div class="input-group-prepend">
+						<span class="input-group-text fa fa-key fa-22-font pt-11"></span>
+					</div>
+					<input class="form-control form-control-lg" type="password" name="password" placeholder="Hasło"></input>
 				</div>
 				
-				<div class="input-group mb-2">
+				<div class="input-group mb-4">
 					<div class="input-group-prepend">
-						<span class="input-group-text fa fa-key fa-22-font"></span>
+						<span class="input-group-text fa fa-key fa-22-font pt-11"></span>
 					</div>
-					<input class="form-control" type="password" name="password" placeholder="Hasło"></input>
+					<input class="form-control form-control-lg" type="password" name="password2" placeholder="Powtórz hasło"></input>
 				</div>
-				
-				<div class="input-group mb-2">
+				<?php
+					if(isset($_SESSION['password_error'])){
+						echo '<p class="text-danger">'.$_SESSION['password_error'].'</p>';
+						unset($_SESSION['password_error']);
+					}
+				?>
+				<div class="input-group mb-4">
 					<div class="input-group-prepend">
-						<span class="input-group-text fa fa-key fa-22-font"></span>
+						<span class="input-group-text fa fa-envelope fa-22-font pt-11"></span>
 					</div>
-					<input class="form-control" type="password" name="password" placeholder="Powtórz hasło"></input>
+					<input class="form-control form-control-lg" type="text" name="email" placeholder="Email"></input>
 				</div>
-				
-				<div class="input-group mb-2">
-					<div class="input-group-prepend">
-						<span class="input-group-text fa fa-envelope fa-22-font"></span>
-					</div>
-					<input class="form-control" type="text" name="email" placeholder="Email"></input>
-				</div>
-				
+				<?php
+					if(isset($_SESSION['email_error'])){
+						echo '<p class="text-danger">'.$_SESSION['email_error'].'</p>';
+						unset($_SESSION['email_error']);
+					}
+				?>
 				<div class="col-sm-12 text-center text-danger mb-2" id="loginFunctionMessage"></div>
-				<a href="register.php" id="signUp" class="btn btn-primary btn-block mb-2">Zarejestruj się</a> 
-			</form>				
+				<input id="signUp" type="submit" class="btn btn-primary btn-block mb-2" value="Zarejestruj się"></input> 
+			</form>		
 			</div>
 			<div class="col-sm-12">
 				<a href="https://pl.freepik.com/darmowe-zdjecie-wektory/pieniadze" class="text-muted pull-right push-down text-sm">Grafika w tle autorstwa natanaelginting - pl.freepik.com</a>
